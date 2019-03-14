e68b0870b7174410c713a677a86e3908adac17ab
Shoptorshi Jamil <sjami002@gmail.com>
2019-03-14 15:56:52 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,14 +1,10 @@
-# def reverse_each_word(sentence)
-#   array = sentence.split(" ")
-#   newArr = []
-#   newArr = array.collect {|word| puts word.reverse}
-#   newArr.join(" ")
-#   end
-# end
+
 
 def reverse_each_word(sentence)
-  reversed = sentence.split(" ").collect do |word|
-  word.reverse
+  array = sentence.split(" ")
+  array.collect do |word|
+    word.reverse
 end
-  reversed.join(" ")
+  array.join(" ")
 end
+
