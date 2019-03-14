f2329ab599d2dec72cf6f2f30fafdb0e09869425
Shoptorshi Jamil <sjami002@gmail.com>
2019-03-14 15:49:25 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,6 +2,6 @@ sentence = "I am a happy person!"
 array = sentence.split(//)
 
 def reverse_each_word(array)
-  array.collect do {|word| puts word.reverse}
+  array.each do {|word| puts word.reverse}
   end
 end
